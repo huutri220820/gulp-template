0fea6b23f449aa77334c55d1aece992e300d1973
Hữu Trí ☘ <huutrimega@gmail.com>
2021-03-05 01:10:32 +0700

    🍟☘☘☘☘☘

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const connect = require("gulp-connect");
+const ts = require("gulp-typescript");
 
 sass.compiler = require("sass");
 
@@ -34,10 +35,26 @@ gulp.task("pug", function () {
 	return gulp.src("./src/pug/**/*.pug").pipe(pug()).pipe(gulp.dest("./dist")).pipe(connect.reload());
 });
 
+gulp.task("typescript", function () {
+	return gulp
+		.src("./src/ts/**/*.ts")
+		.pipe(sourcemaps.init())
+		.pipe(
+			ts({
+				declaration: true,
+				module: "commonjs",
+			})
+		)
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("./dist/js"))
+		.pipe(connect.reload());
+});
+
 gulp.task("watch", function () {
 	gulp.watch("./src/scss/**/*.scss", gulp.series("scss"));
 	gulp.watch("./src/pug/**/*.pug", gulp.series("pug"));
+	gulp.watch("./src/ts/**/*.ts", gulp.series("typescript"));
 });
-gulp.task("watch", gulp.series(["connect", "watch"]));
 
-gulp.task("build", gulp.series(["scss", "pug"]));
+gulp.task("start", gulp.series(["connect", "watch"]));
+gulp.task("build", gulp.series(["scss", "pug", "typescript"]));
